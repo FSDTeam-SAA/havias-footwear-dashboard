1a408bb2faa0b424eff94ce7eb50885a65dcb2ed
mehedishishir0 <mehedihasanshishir@gmail.com>
2025-08-26 17:57:56 +0600

    fixup

--- src/app/(dashboard)/categories/page.tsx
+++ src/app/(dashboard)/categories/page.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import Title from '../_components/Title'
 import CategoryTableComponent from './_components/categoryComponents'
 
 const Page = () => {
     return (
         <div>
-            <Title title='Categories List' active='Dashboard > Categories > List' />
+          
             <CategoryTableComponent/>
         </div>
     )
